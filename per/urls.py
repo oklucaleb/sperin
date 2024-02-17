ccc74abebddaf17b116c8de9151fab8342d57dd9
oklucaleb <oklucaleb@gmail.com>
2024-02-17 23:50:06 +0000

    research

--- per/urls.py
+++ per/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('category',views.category,name='category'),
+    path('work',views.work,name='work'),
 ]
--- per/views.py
+++ per/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, 'index.html')
+
+def about(request):
+    return render (request, 'about.html')
+
+def category(request):
+    return render (request, 'category.html')
+
+def work(request):
+    return render (request, 'work.html')
